2ed685cb63c03111267304892008f9d6a2a8529b
vijayskk <vijayskk007@gmail.com>
2022-06-26 07:09:27 +0530

    Post Processing tried

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { PointLightHelper } from 'three';
 const scene = new THREE.Scene();
 
  
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight , 0.1 , 1000);
 
 
